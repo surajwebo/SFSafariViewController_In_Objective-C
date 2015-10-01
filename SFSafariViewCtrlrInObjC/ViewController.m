3f315cc0a0bfccf712f630ef396ed54e9a080f14
surajwebo <surajmirajkar@rediffmail.com>
2015-10-01 15:25:35 +0530

    committed changes

--- SFSafariViewCtrlrInObjC/ViewController.m
+++ SFSafariViewCtrlrInObjC/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    SFSafariViewController *sfSafariViewController;
+}
 
 @end
 
@@ -16,12 +18,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:NO];
+
+    void (^loadSFSafariViewControllerBlock)(NSURL *) = ^(NSURL *url) {
+        sfSafariViewController = [[SFSafariViewController alloc] initWithURL:url entersReaderIfAvailable:YES];
+        sfSafariViewController.delegate = self;
+        [self presentViewController:sfSafariViewController animated:YES completion:nil];
+    };
+    NSURL *url = [NSURL URLWithString:@"http://objectivecwithsuraj.blogspot.com/"];
+    loadSFSafariViewControllerBlock(url);
+}
+
+#pragma mark - SFSafariViewController Delegate Methods
+-(void)safariViewController:(SFSafariViewController *)controller didCompleteInitialLoad:(BOOL)didLoadSuccessfully {
+    // Load finished
+    if (didLoadSuccessfully) {
+        NSLog(@"SafariViewController: Loading of URl finished");
+    }
+}
+
+-(void)safariViewControllerDidFinish:(SFSafariViewController *)controller {
+    // Done button pressed
+    NSLog(@"safariViewController: Done button pressed");
+    [sfSafariViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
